f7079430d4029a95640b2a1f790e5caad1ec96bc
1milosz2 <1milosz2@gmail.com>
2018-08-11 17:26:34 +0200

    Google Guice for Dependency injection added

--- src/main/java/com/h8/todoapp/Main.java
+++ src/main/java/com/h8/todoapp/Main.java
@@ -5,16 +5,10 @@ import com.google.inject.Injector;
 
 public class Main {
 
-    private static TodoEntryController controller;
-
     public static void main(String... args) {
         if (System.console() != null) {
             Injector injector = Guice.createInjector(new ToDoEntryControllerModule());
-            TodoEntryRepository repository = injector.getInstance(TodoEntryRepository.class);
-//            TodoEntryRepository repository = new TodoEntryRepository();
-            TodoEntryService service = new TodoEntryService(repository);
-            TodoEntryController controller = new TodoEntryController(service);
-//            TodoEntryController controller = injector.getInstance(TodoEntryController.class);
+            TodoEntryController controller = injector.getInstance(TodoEntryController.class);
             while (controller.readCommand());
         } else {
             System.out.println("Application requires console. Bye!");
--- src/main/java/com/h8/todoapp/ToDoEntryControllerModule.java
+++ src/main/java/com/h8/todoapp/ToDoEntryControllerModule.java
@@ -5,14 +5,5 @@ import com.google.inject.AbstractModule;
 public class ToDoEntryControllerModule extends AbstractModule {
     @Override
     protected void configure() {
-bind(TodoEntryRepository.class).toInstance(new TodoEntryRepository());
-//bind(TodoEntryService.class).toInstance(new TodoEntryService(new TodoEntryRepository()));
-
     }
-
-//    @Provides
-//    TodoEntryController provideController () {
-//        TodoEntryController controller = new TodoEntryController(new TodoEntryService(new TodoEntryRepository()));
-//        return controller;
-//    }
 }
